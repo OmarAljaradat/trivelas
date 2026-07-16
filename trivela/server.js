9c45ce186c0e240ad1e7de4a0674aa7be8dd0838
omarj <omarj@example.com>
2026-07-16 20:23:47 +0300

    chore: change server port from 3000 to 3500

--- trivela/server.js
+++ trivela/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const crypto = require('crypto');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3500;
 const DB_PATH = path.join(__dirname, 'database.json');
 
 app.use(express.json());
